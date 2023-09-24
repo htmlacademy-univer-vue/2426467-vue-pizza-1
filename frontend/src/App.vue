287616671913ac29f326bdcd298ffabb2e6cda12
gigafiga21 <gigafiga21@gmail.com>
2023-09-24 17:22:24 +0000

    feat: use SASS styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,33 @@
   </app-layout>
 </template>
 
+<style lang="scss">
+@import "@/assets/scss/ds-system/ds.scss";
+
+body {
+  display: flex;
+  flex-direction: column;
+  min-width: 960px;
+  min-height: 100vh;
+  margin: 0;
+  color: $black;
+  background-color: $white;
+  font-family: "Roboto", Arial, sans-serif;
+  font-size: 14px;
+  line-height: 16px;
+  text-rendering: optimizeLegibility;
+}
+
+a {
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
+
 <script setup>
 import { HomeView } from "@/views";
 import { AppLayout } from "@/layouts";
