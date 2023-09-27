16ad9b9a06592219bbfedf2fd8bca46d665cff7e
gigafiga21 <gigafiga21@gmail.com>
2023-09-27 07:53:50 +0000

    feat: 'VariableButton' component

--- frontend/src/common/components/index.js
+++ frontend/src/common/components/index.js
@@ -1,3 +1,4 @@
 export { default as VariableTitle } from "./VariableTitle.vue";
 export { default as SheetCard } from "./SheetCard.vue";
 export { default as RadioButton } from "./RadioButton.vue";
+export { default as VariableButton } from "./VariableButton.vue";
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -125,7 +125,7 @@
 
           <div class="content__result">
             <p>Итого: 0 ₽</p>
-            <button type="button" class="button" disabled>Готовьте!</button>
+            <VariableButton disabled>Готовьте!</VariableButton>
           </div>
         </div>
       </div>
@@ -143,7 +143,12 @@ import sauces from "../mocks/sauces.json";
 import INGREDIENTS from "../common/data/ingredients.js";
 import ingredients from "../mocks/ingredients.json";
 
-import { VariableTitle, SheetCard, RadioButton } from "@/common/components";
+import {
+  VariableTitle,
+  SheetCard,
+  RadioButton,
+  VariableButton,
+} from "@/common/components";
 </script>
 
 <style lang="scss" scoped>
@@ -814,109 +819,6 @@ import { VariableTitle, SheetCard, RadioButton } from "@/common/components";
       margin-right: 16px;
       white-space: nowrap;
     }
-  }
-}
-
-.button {
-  $bl: &;
-  @include b-s18-h21;
-
-  font-family: inherit;
-  display: block;
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  cursor: pointer;
-  transition: 0.3s;
-  text-align: center;
-  color: $white;
-  border: none;
-  border-radius: 8px;
-  outline: none;
-  box-shadow: $shadow-medium;
-
-  background-color: $green-500;
-
-  &:hover:not(:active):not(:disabled) {
-    background-color: $green-400;
-  }
-
-  &:active:not(:disabled) {
-    background-color: $green-600;
-  }
-
-  &:focus:not(:disabled) {
-    opacity: 0.5;
-  }
-
-  &:disabled {
-    background-color: $green-300;
-    color: rgba($white, 0.2);
-    cursor: default;
-  }
-
-  &--border {
-    background-color: transparent;
-    border: 1px solid $green-500;
-    color: $black;
-    box-shadow: none;
-
-    &:hover:not(:active):not(:disabled) {
-      color: $green-500;
-      border-color: $green-500;
-      background-color: transparent;
-    }
-
-    &:active:not(:disabled) {
-      color: $green-600;
-      border-color: $green-600;
-      background-color: transparent;
-    }
-
-    &:disabled {
-      opacity: 0.5;
-    }
-  }
-
-  &--transparent {
-    @include b-s14-h16;
-    background-color: transparent;
-    box-shadow: none;
-    color: $black;
-
-    &:hover:not(:active):not(:disabled) {
-      color: $red-800;
-      background-color: transparent;
-    }
-
-    &:active:not(:disabled) {
-      color: $red-900;
-      background-color: transparent;
-    }
-
-    &:disabled {
-      opacity: 0.25;
-    }
-  }
-
-  &--arrow {
-    &::before {
-      content: "";
-      background-image: url("@/assets/img/button-arrow.svg");
-      background-position: center;
-      background-repeat: no-repeat;
-      margin-right: 16px;
-      width: 18px;
-      height: 18px;
-      display: inline-block;
-      vertical-align: middle;
-      transform: translateY(-1px);
-    }
-  }
-
-  &--white {
-    background-color: $white;
-    color: $green-500;
   }
 }
 </style>
