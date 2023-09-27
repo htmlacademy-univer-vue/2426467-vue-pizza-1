019ece387eed57c152cffe7e850a2beed743fdfa
gigafiga21 <gigafiga21@gmail.com>
2023-09-27 08:28:02 +0000

    feat: 'CounterInput' component

--- frontend/src/common/components/index.js
+++ frontend/src/common/components/index.js
@@ -3,3 +3,4 @@ export { default as SheetCard } from "./SheetCard.vue";
 export { default as RadioButton } from "./RadioButton.vue";
 export { default as VariableButton } from "./VariableButton.vue";
 export { default as VariableInput } from "./VariableInput.vue";
+export { default as CounterInput } from "./CounterInput.vue";
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -76,27 +76,7 @@
                     >{{ ingredient.name }}</span
                   >
 
-                  <div class="counter counter--orange ingredients__counter">
-                    <button
-                      type="button"
-                      class="counter__button counter__button--minus"
-                      disabled
-                    >
-                      <span class="visually-hidden">Меньше</span>
-                    </button>
-                    <input
-                      type="text"
-                      name="counter"
-                      class="counter__input"
-                      value="0"
-                    />
-                    <button
-                      type="button"
-                      class="counter__button counter__button--plus"
-                    >
-                      <span class="visually-hidden">Больше</span>
-                    </button>
-                  </div>
+                  <counter-input class="ingredients__counter" />
                 </li>
               </ul>
             </div>
@@ -146,6 +126,7 @@ import {
   RadioButton,
   VariableButton,
   VariableInput,
+  CounterInput,
 } from "@/common/components";
 </script>
 
@@ -471,136 +452,6 @@ import {
 
   &--salmon::before {
     background-image: url("@/assets/img/filling/salmon.svg");
-  }
-}
-
-.counter {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.counter__button {
-  $el: &;
-  $size_icon: 50%;
-
-  position: relative;
-  display: block;
-  width: 16px;
-  height: 16px;
-  margin: 0;
-  padding: 0;
-  cursor: pointer;
-  transition: 0.3s;
-  border: none;
-  border-radius: 50%;
-  outline: none;
-
-  &--minus {
-    background-color: $purple-100;
-
-    &::before {
-      @include p_center-all;
-
-      width: $size_icon;
-      height: 2px;
-      content: "";
-      border-radius: 2px;
-      background-color: $black;
-    }
-
-    &:hover:not(:active):not(:disabled) {
-      background-color: $purple-200;
-    }
-
-    &:active:not(:disabled) {
-      background-color: $purple-300;
-    }
-
-    &:focus:not(:disabled) {
-      box-shadow: $shadow-regular;
-    }
-
-    &:disabled {
-      cursor: default;
-
-      &::before {
-        opacity: 0.1;
-      }
-    }
-  }
-
-  &--plus {
-    background-color: $green-500;
-
-    &::before {
-      @include p_center-all;
-
-      width: $size_icon;
-      height: 2px;
-      content: "";
-      border-radius: 2px;
-      background-color: $white;
-    }
-
-    &::after {
-      @include p_center-all;
-
-      width: $size_icon;
-      height: 2px;
-      content: "";
-      transform: translate(-50%, -50%) rotate(90deg);
-      border-radius: 2px;
-      background-color: $white;
-    }
-
-    &:hover:not(:active):not(:disabled) {
-      background-color: $green-400;
-    }
-
-    &:active:not(:disabled) {
-      background-color: $green-600;
-    }
-
-    &:focus:not(:disabled) {
-      box-shadow: $shadow-regular;
-    }
-
-    &:disabled {
-      cursor: default;
-      opacity: 0.3;
-    }
-  }
-
-  &--orange {
-    background-color: $orange-200;
-
-    &:hover:not(:active):not(:disabled) {
-      background-color: $orange-100;
-    }
-
-    &:active:not(:disabled) {
-      background-color: $orange-300;
-    }
-  }
-}
-
-.counter__input {
-  @include r-s14-h16;
-
-  box-sizing: border-box;
-  width: 22px;
-  margin: 0;
-  padding: 0 3px;
-  text-align: center;
-  color: $black;
-  border: none;
-  border-radius: 10px;
-  outline: none;
-  background-color: transparent;
-
-  &:focus {
-    box-shadow: inset $shadow-regular;
   }
 }
 
