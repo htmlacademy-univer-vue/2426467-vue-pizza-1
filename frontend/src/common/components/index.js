e3e50c3a1ce7196c978d392d2e73bfad106df53f
gigafiga21 <gigafiga21@gmail.com>
2023-09-27 08:00:47 +0000

    feat: 'VariableInput' component

--- frontend/src/common/components/index.js
+++ frontend/src/common/components/index.js
@@ -2,3 +2,4 @@ export { default as VariableTitle } from "./VariableTitle.vue";
 export { default as SheetCard } from "./SheetCard.vue";
 export { default as RadioButton } from "./RadioButton.vue";
 export { default as VariableButton } from "./VariableButton.vue";
+export { default as VariableInput } from "./VariableInput.vue";
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -104,14 +104,11 @@
         </div>
 
         <div class="content__pizza">
-          <label class="input">
-            <span class="visually-hidden">Название пиццы</span>
-            <input
-              type="text"
-              name="pizza_name"
-              placeholder="Введите название пиццы"
-            />
-          </label>
+          <variable-input
+            name="pizzan_name"
+            label="Название пиццы"
+            placeholder="Введите название пиццы"
+          />
 
           <div class="content__constructor">
             <div class="pizza pizza--foundation--big-tomato">
@@ -148,6 +145,7 @@ import {
   SheetCard,
   RadioButton,
   VariableButton,
+  VariableInput,
 } from "@/common/components";
 </script>
 
@@ -769,56 +767,6 @@ import {
   &--tomatoes.pizza__filling--second::before,
   &--tomatoes.pizza__filling--third::after {
     background-image: url("@/assets/img/filling-big/tomatoes.svg");
-  }
-}
-
-.input {
-  display: block;
-
-  span {
-    @include r-s14-h16;
-
-    display: block;
-    margin-bottom: 4px;
-  }
-
-  input {
-    @include r-s16-h19;
-
-    display: block;
-    box-sizing: border-box;
-    width: 100%;
-    margin: 0;
-    padding: 8px 16px;
-    transition: 0.3s;
-    color: $black;
-    border: 1px solid $purple-400;
-    border-radius: 8px;
-    outline: none;
-    background-color: $white;
-    font-family: inherit;
-
-    &:focus {
-      border-color: $green-500;
-    }
-  }
-
-  &:hover {
-    input {
-      border-color: $black;
-    }
-  }
-
-  &--big-label {
-    display: flex;
-    align-items: center;
-
-    span {
-      @include b-s16-h19;
-
-      margin-right: 16px;
-      white-space: nowrap;
-    }
   }
 }
 </style>
