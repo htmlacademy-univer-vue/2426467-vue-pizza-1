e21d13b1b545ab9b69b5eac451eb74f15444cc22
gigafiga21 <gigafiga21@gmail.com>
2023-10-07 12:30:01 +0000

    feat: AppDrag and AppDrop components

--- frontend/src/common/components/index.js
+++ frontend/src/common/components/index.js
@@ -4,3 +4,5 @@ export { default as RadioButton } from "./RadioButton.vue";
 export { default as VariableButton } from "./VariableButton.vue";
 export { default as VariableInput } from "./VariableInput.vue";
 export { default as CounterInput } from "./CounterInput.vue";
+export { default as AppDrag } from "./AppDrag.vue";
+export { default as AppDrop } from "./AppDrop.vue";
